db20375d38c27c9a02ffb6160f73488548d5b04f
Zoltan Meze <zoltan.meze@gmail.com>
2021-12-06 06:50:25 +0100

    Day 06
    Signed-off-by: Zoltan Meze <zoltan.meze@gmail.com>

--- src/main/java/com/github/zoltanmeze/aoc/day06/Day06.java
+++ src/main/java/com/github/zoltanmeze/aoc/day06/Day06.java
@@ -1,4 +1,4 @@
-package com.github.zoltanmeze.aoc.day01;
+package com.github.zoltanmeze.aoc.day06;
 
 import com.github.zoltanmeze.aoc.utilities.ResourceUtils;
 import lombok.SneakyThrows;
@@ -25,42 +25,60 @@ public class Day06 implements Runnable {
 
     public Object partOne() {
         List<Integer> input = parseInput();
-        int count = 0;
-        int prev = input.get(0);
-        for (int i = 1; i < input.size(); i++) {
-            if (input.get(i) > prev) {
-                count++;
-            }
-            prev = input.get(i);
-        }
-        return count;
+        return calc(input, 80);
     }
-
 
     public Object partTwo() {
         List<Integer> input = parseInput();
-        int count = 0;
-        int prev = input.get(0) + input.get(1) + input.get(2);
-        for (int i = 1; i < input.size() - 2; i++) {
-            int next = prev - input.get(i - 1) + input.get(i + 2);
-            if (next > prev) {
-                count++;
-            }
-            prev = next;
+        return calc(input, 256);
+    }
+
+    private void printArray(long[] arr, int start) {
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[(i + start) % arr.length] + (i != arr.length - 1 ? "," : ""));
         }
-        return count;
+        System.out.println("]");
+    }
+
+    public Object calc(List<Integer> input, int days) {
+        int size = 9;
+        long[] ages = new long[size];
+        for (Integer a : input) {
+            ages[a]++;
+        }
+        /*for (int i = 0; i < days; i++) {
+            long[] agesnew = new long[ages.length];
+            arraycopy(ages, 1, agesnew, 0, ages.length - 1); // copy array with shift to 1 left
+            agesnew[6] += ages[0];
+            agesnew[8] = ages[0];
+            ages = agesnew;
+            printArray(ages, 0);
+        }*/
+        // Shift the starting index, not the whole array
+        for (int i = 0; i < days; i++) {
+            // eq to ages[(i + 6) % size] += ages[(i - 1) % size] with i = 1 and i <= days
+            ages[(i + 7) % size] += ages[i % size];
+        }
+        long num = 0;
+        for (long age : ages) {
+            num += age;
+        }
+        return num;
     }
 
     @SneakyThrows
     public List<Integer> parseInput() {
-        File file = ResourceUtils.getResourceFile("day01.txt");
+        File file = ResourceUtils.getResourceFile("day06.txt");
         try (
             FileReader fileReader = new FileReader(file);
             Scanner scanner = new Scanner(fileReader)
         ) {
+            scanner.useDelimiter(",");
             List<Integer> results = new ArrayList<>();
             while (scanner.hasNext()) {
-                results.add(Integer.valueOf(scanner.nextLine()));
+                String num = scanner.next().trim();
+                results.add(Integer.parseInt(num));
             }
             return results;
         }
